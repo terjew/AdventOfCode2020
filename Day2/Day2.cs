9cfa5c2f9cfefb6732739800b494b45b00b4edd4
Terje Wiesener <wiesener@gmail.com>
2020-12-02 14:22:27 +0100

    Day 2 solution

---
+++ Day2/Day2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Utilities;
+
+namespace Day2
+{
+    class PasswordLine
+    {
+        public int min;
+        public int max;
+        public char c;
+        public string password;
+
+        public bool IsValidRule1()
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] == c) count++;
+            }
+            if (count < min || count > max) return false;
+            return true;
+        }
+
+
+        public bool IsValidRule2()
+        {
+            return password[min - 1] == c ^ password[max - 1] == c;
+        }
+    }
+
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            int valid1 = 0;
+            int valid2 = 0;
+            Performance.TimeRun("Read and check passwords", () =>
+            {
+                var passwords = ParsePasswords("puzzle.txt");
+                valid1 = 0;
+                valid2 = 0;
+                for (int i = 0; i < passwords.Count; i++)
+                {
+                    if (passwords[i].IsValidRule1()) valid1++;
+                    if (passwords[i].IsValidRule2()) valid2++;
+                }
+            });
+            Console.WriteLine($"{valid1} valid passwords found using rule 1");
+            Console.WriteLine($"{valid2} valid passwords found using rule 2");
+        }
+
+        static List<PasswordLine> ParsePasswords(string path)
+        {
+            List<PasswordLine> lines = new List<PasswordLine>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    var line = new PasswordLine();
+
+                    int num = 0;
+                    int i = 0;
+                    char c = s[i];
+                    while(c != '-')
+                    {
+                        num = num * 10 + (c - '0');
+                        c = s[++i];
+                    }
+                    line.min = num;
+            
+                    num = 0;
+                    c = s[++i];
+                    while (c != ' ')
+                    {
+                        num = num * 10 + (c - '0');
+                        c = s[++i];
+                    }
+                    line.max = num;
+
+                    line.c = s[++i];
+
+                    line.password = s.Substring(i+3);
+                    lines.Add(line);
+                }
+            }
+            return lines;
+        }
+
+        static List<PasswordLine> ParsePasswordsRegex(string path)
+        {
+            List<PasswordLine> passwords = new List<PasswordLine>();
+            Regex re = new Regex(@"(?<min>\d+)-(?<max>\d+) (?<char>\w): (?<password>.*)", RegexOptions.Compiled);
+            int i = 0;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    var match = re.Match(s);
+                    var password = new PasswordLine();
+                    password.min = match.GetInt("min");
+                    password.max = match.GetInt("max");
+                    password.c = match.Groups["char"].ToString()[0];
+                    password.password = match.Groups["password"].ToString();
+                    passwords.Add(password);
+                    i++;
+                }
+            }
+            return passwords;
+        }
+
+    }
+}
---
+++ Utilities/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Extensions
+    {
+        public static int GetInt(this Match m, string groupName)
+        {
+            int y = 0;
+            var s = m.Groups[groupName].ToString();
+            for (int i = 0; i < s.Length; i++)
+                y = y * 10 + (s[i] - '0');
+            return y;
+        }
+    }
+}
